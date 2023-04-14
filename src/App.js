1c20d9d38b7a7418f8badb77ba399263b5a310cf
Yahya Mahmud Orhan <e1327280@student.tuwien.ac.at>
2023-04-14 12:57:10 +0200

    added tailwindcss

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +18,11 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1 className="mt-3 p-3 italic text-3xl font-bold border-4 border-red-500/50 rounded">
+          El Barto was here. <span className="text-base not-italic">(This text is styled using tailwindcss)</span>
+        </h1>
+
       </header>
     </div>
   );
